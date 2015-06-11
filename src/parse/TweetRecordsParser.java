ea6d1af4e73834b8153608598984c1aded71e036
harishankar-vishwanathan <harishankar.vishwanathan@gmail.com>
2015-06-11 19:45:37 -0400

    getFollowers(userId) now working

--- src/parse/TweetRecordsParser.java
+++ src/parse/TweetRecordsParser.java
@@ -10,34 +10,41 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import collect.ConfigBuilder;
+import twitter4j.IDs;
 import twitter4j.PagableResponseList;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
 
 
 public class TweetRecordsParser {
 	
 	static HashMap<Long, Integer> hashMap = new HashMap<Long, Integer>();
 	static ArrayList<Long> followers = new ArrayList<Long>();
+	static Twitter twitter;
 	
 	public static void main(String[] args){
 		
-		parseFile();
-		getFollowers(1234); 
+		twitter = new TwitterFactory(collect.ConfigBuilder.getConfig()).getInstance();
+		
+		parseFileIntoHashMap("428333");
+		writeHashMapToFile("428333_map_novalidation");
+		getFollowers(428333L); 
 		validate();
-		writeToFile();
+		writeHashMapToFile("428333_map_aftervalidation");			
 		
 	}
 	
-	public static void parseFile(){
+	public static void parseFileIntoHashMap(String filename){
 		String line;
 		try{
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("cnnbrk_round1.txt")));
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
 			
 			while ((line = bufferedReader.readLine()) !=null) {
-				System.out.println(line);
 				String[] tokens = line.split(",");
 				int length = tokens.length;
 				long userId = Long.parseLong(tokens[length - 5]);
@@ -56,31 +63,44 @@ public class TweetRecordsParser {
 	
 	public static void getFollowers(long userId){
 		
-		Twitter twitter = new TwitterFactory(collect.ConfigBuilder.getConfig()).getInstance();
-		
-		try{
-			PagableResponseList<User> pagableFollowers;
-	        pagableFollowers = twitter.getFollowersList(userId, -1);   	
-			while(pagableFollowers.hasNext()){
-			   for (User user : pagableFollowers) {
-                    followers.add(user.getId());
-                }
-		   }
-	    }catch(TwitterException twitterException){
-		    	twitterException.printStackTrace();
+	/*	 try {
+	            // get followers
+	            long cursor = -1;
+	            PagableResponseList<User> pagableFollowers;
+	            do {
+	                pagableFollowers = twitter.getFollowersList("cnnbrk", cursor);
+	                for (User user : pagableFollowers) {
+	                	followers.add(user.getId()); // ArrayList<User>
+	                	System.out.println(user.getId());
+	                }
+	            } while ((cursor = pagableFollowers.getNextCursor()) != 0);
+
+	        } catch (TwitterException twitterException) {
+	            twitterException.printStackTrace();
+	        }
+		 
+*/		 
+		 try {
+		        IDs ids = twitter.getFollowersIDs(userId, -1);
+		        do {
+		            for (long id : ids.getIDs()) {               
+		                System.out.println(id);
+		            }
+		        } while (ids.hasNext());
+
+		    } catch (Exception e) {
+		        e.printStackTrace();
 		    }
 	}
 	
 	public static void validate(){
 		
-		
-		
 	}
 	
-	public static void writeToFile(){
+	public static void writeHashMapToFile(String filename){
 		
 		try{
-			PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("hashMap.txt", true)));
+			PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)));
 			for(HashMap.Entry<Long, Integer> entry : hashMap.entrySet()){
 			    printWriter.println(entry.getKey() + "," + entry.getValue());
 			}
