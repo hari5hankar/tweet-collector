ec58d9196a7b43feb942f67023b543cec6ed9c58
harishankar-vishwanathan <harishankar.vishwanathan@gmail.com>
2015-06-12 21:44:00 -0700

    validaiton of all retweeting users against followers

--- src/collect/ConfigBuilder.java
+++ src/collect/ConfigBuilder.java
@@ -6,26 +6,28 @@ public class ConfigBuilder {
 
         private static Configuration configuration;
 
-
+/*
         //Abhishek's keys
-        private static final String consumerKeyStr = "6xApxD6P3xPGa6fGEqeezvBTh";
-        private static final String consumerSecretStr ="sZQtcKOlTl9AtTg6ZvMcmuF31EoVk7d7tNqhU8LlwOIcMjMRyP";
-        private static final String accessTokenStr ="2584378076-QaUNEp9CY144WwPJXYejRtdi5mjEQJEGeJyUeZ8";
-        private static final String accessTokenSecretStr ="ZB8AIvzEJZvjkedlJMckwHZXabaD3vvh9PTUDa0uhxylQ";
+        public static final String consumerKeyStr = "6xApxD6P3xPGa6fGEqeezvBTh";
+        public static final String consumerSecretStr ="sZQtcKOlTl9AtTg6ZvMcmuF31EoVk7d7tNqhU8LlwOIcMjMRyP";
+        public static final String accessTokenStr ="2584378076-QaUNEp9CY144WwPJXYejRtdi5mjEQJEGeJyUeZ8";
+        public static final String accessTokenSecretStr ="ZB8AIvzEJZvjkedlJMckwHZXabaD3vvh9PTUDa0uhxylQ";
 
-/*
-        //Omkar's keys
-        private static final String consumerKeyStr = "6kRVFFfsFLfzPH4IUTlvgulbJ";
-        private static final String consumerSecretStr = "VwzVBO5ygloqoyRMl9YWSFpiuV5arjO9HpYr648Ci3WEoGD7jl";
-        private static final String accessTokenStr ="134179394-f7YpC2Pxd5QXucqoaWg3UAmH1Mc793bmYLhhlPxD";
-        private static final String accessTokenSecretStr ="23jbLXADuXY2TA5TUGQkUayPtEqK3Dr5BelH00SKLG6Si";
 */
-/*
-        //Harishankar's keys:
-        private static final String consumerKeyStr = "RCVjvrIN1nHF9UuddyWIw0GkO";
-        private static final String consumerSecretStr ="0D0BeIdeVv8jp7pPBLrhmnas7dlHCEjxxlFLFinFtZWzkut3PQ";
-        private static final String accessTokenStr ="2972666885-riSRlSUrmIOZH10msEa7bxOLj4BJVqKaI3GkUzC";
-        private static final String accessTokenSecretStr ="pfHalw9HmwFtoVpongAFKod9C4oCzWCGCjeihbvUfiWXe";
+
+        //Omkar's keys
+        public static final String consumerKeyStr = "6kRVFFfsFLfzPH4IUTlvgulbJ";
+        public static final String consumerSecretStr = "VwzVBO5ygloqoyRMl9YWSFpiuV5arjO9HpYr648Ci3WEoGD7jl";
+        public static final String accessTokenStr ="134179394-f7YpC2Pxd5QXucqoaWg3UAmH1Mc793bmYLhhlPxD";
+        public static final String accessTokenSecretStr ="23jbLXADuXY2TA5TUGQkUayPtEqK3Dr5BelH00SKLG6Si";
+
+
+        
+/*        //Harishankar's keys:
+        public static final String consumerKeyStr = "RCVjvrIN1nHF9UuddyWIw0GkO";
+        public static final String consumerSecretStr ="0D0BeIdeVv8jp7pPBLrhmnas7dlHCEjxxlFLFinFtZWzkut3PQ";
+        public static final String accessTokenStr ="2972666885-riSRlSUrmIOZH10msEa7bxOLj4BJVqKaI3GkUzC";
+        public static final String accessTokenSecretStr ="pfHalw9HmwFtoVpongAFKod9C4oCzWCGCjeihbvUfiWXe";
 */
         static {
                 ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
--- src/parse/TweetRecordsParser.java
+++ src/parse/TweetRecordsParser.java
@@ -3,43 +3,56 @@ package parse;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import collect.ConfigBuilder;
 import twitter4j.IDs;
-import twitter4j.PagableResponseList;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
-import twitter4j.auth.AccessToken;
-import twitter4j.conf.ConfigurationBuilder;
-
 
 public class TweetRecordsParser {
 	
-	static HashMap<Long, Integer> hashMap = new HashMap<Long, Integer>();
-	static ArrayList<Long> followers = new ArrayList<Long>();
-	static Twitter twitter;
-	
-	public static void main(String[] args){
-		
+	HashMap<Long, Integer> userIDMap; //Map <userId, number of retweets>
+	HashMap<Long, Integer> validatedUserIDMap; //Map <userId, number of retweets>
+	Twitter twitter;
+
+	public TweetRecordsParser() {
+		System.out.println("TweetRecordsParser()");
+		userIDMap = new HashMap<Long, Integer>();
+		validatedUserIDMap= new HashMap<Long, Integer>();
 		twitter = new TwitterFactory(collect.ConfigBuilder.getConfig()).getInstance();
-		
-		parseFileIntoHashMap("428333");
-		writeHashMapToFile("428333_map_novalidation");
-		getFollowers(428333L); 
-		validate();
-		writeHashMapToFile("428333_map_aftervalidation");			
-		
 	}
 	
-	public static void parseFileIntoHashMap(String filename){
+	//TODO new folder for each round
+	
+	/*
+	 * will generate one file, for each user specified in UserIDs
+	 * each file will contain a list: <follower Id, number of retweets>, 
+	 */
+	
+	public void parse(Long ... userIDsArray) {
+		System.out.println("parse()");
+		
+		for(Long mainUserID : userIDsArray) {
+			String filename = Long.toString(mainUserID);
+			parseFileIntoMap(filename);
+			writeHashMapToFile(userIDMap, filename+ "_NV"); //test, write non validated 
+			getFollowersAndValidate(mainUserID); 
+			writeHashMapToFile(validatedUserIDMap, filename+ "_V");
+		}
+	}
+	
+	private void parseFileIntoMap(String filename){
+		System.out.println("parseFileIntoMap()");		
 		String line;
 		try{
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
@@ -47,61 +60,70 @@ public class TweetRecordsParser {
 			while ((line = bufferedReader.readLine()) !=null) {
 				String[] tokens = line.split(",");
 				int length = tokens.length;
-				long userId = Long.parseLong(tokens[length - 5]);
-				if (hashMap.get(userId) == null){
-					hashMap.put(userId, 1);	
-				} else{
-					hashMap.put(userId, hashMap.get(userId) +1 );
+				//5th from last element is the userID in our String
+				long userID = Long.parseLong(tokens[length - 5]);
+				
+				if (userIDMap.get(userID) == null){
+					userIDMap.put(userID, 1);	
+				} else {
+					userIDMap.put(userID, userIDMap.get(userID) +1 );
 				}
-			}
 			
+			}
 			bufferedReader.close();
 		}catch(IOException ioException){
 			ioException.printStackTrace();
 		}
 	}
 	
-	public static void getFollowers(long userId){
-		
-	/*	 try {
-	            // get followers
-	            long cursor = -1;
-	            PagableResponseList<User> pagableFollowers;
-	            do {
-	                pagableFollowers = twitter.getFollowersList("cnnbrk", cursor);
-	                for (User user : pagableFollowers) {
-	                	followers.add(user.getId()); // ArrayList<User>
-	                	System.out.println(user.getId());
-	                }
-	            } while ((cursor = pagableFollowers.getNextCursor()) != 0);
-
-	        } catch (TwitterException twitterException) {
-	            twitterException.printStackTrace();
-	        }
-		 
-*/		 
+	private void getFollowersAndValidate(long mainUserID){
+		System.out.println("getFollowersAndValidate()");
 		 try {
-		        IDs ids = twitter.getFollowersIDs(userId, -1);
-		        do {
-		            for (long id : ids.getIDs()) {               
-		                System.out.println(id);
-		            }
-		        } while (ids.hasNext());
-
-		    } catch (Exception e) {
+		        IDs ids = twitter.getFollowersIDs(mainUserID, -1);
+		        String filename = Long.toString(mainUserID);
+    			do{
+    				for (long userID : ids.getIDs()) { 
+    					if(userIDMap.containsKey(userID)) {
+    		 				validatedUserIDMap.put(userID , userIDMap.get(userID));
+    		 			}
+    				}
+	            } while (ids.hasNext());
+    			
+		 }catch (Exception e) {
 		        e.printStackTrace();
-		    }
+		 }
 	}
+
+/*	//validates the Map<userID, number of retweets> against List<followers>
+	private void validate(Long mainUserID) {
 	
-	public static void validate(){
-		
-	}
-	
-	public static void writeHashMapToFile(String filename){
+		System.out.println("validate()");
+		String filename = Long.toString(mainUserID);
+		followersList.clear();
 		
 		try{
+			ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filename + "_L"));
+			
+			while(true){
+	 			long userID = objectInputStream.readLong();
+	 			if(userIDMap.containsKey(userID)){
+	 				validatedUserIDMap.put(userID , userIDMap.get(userID));
+	 			}
+	 			objectInputStream.close();
+			}
+			
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+
+	}
+	
+*/	
+	private void writeHashMapToFile(HashMap<Long, Integer> map, String filename){
+		System.out.println("writeHashMapToFile()");
+		try{
 			PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)));
-			for(HashMap.Entry<Long, Integer> entry : hashMap.entrySet()){
+			for(HashMap.Entry<Long, Integer> entry : map.entrySet()){
 			    printWriter.println(entry.getKey() + "," + entry.getValue());
 			}
 			printWriter.close();
@@ -110,4 +132,52 @@ public class TweetRecordsParser {
 		}	
 	}
 	
+/*	private void writeListToFile(String filename) {
+		System.out.println("writeListToFile()");
+		try{
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filename));
+			for(Long userID: followersList){
+				objectOutputStream.writeLong(userID);
+			}
+			objectOutputStream.close();
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}	
+	}*/
+	
 }
+
+
+/*
+try{
+	//PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)));
+	for(Long l: followersList){
+		//printWriter.println(l);
+	}
+	objectOutputStream.close();
+}catch(IOException ioException){
+	ioException.printStackTrace();
+}
+*/
+
+/*
+try{
+	BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+	
+	while ((line = bufferedReader.readLine()) !=null) {
+			followersList.add(Long.parseLong(line)); 
+			counter++;
+			if(counter == 10000000){
+				for(Long userID : userIDMap.keySet()){
+					if ( !(followersList.contains(userID)) ){
+						userIDMap.remove(userID);
+					}
+				}
+				counter = 0L;
+			}
+	}
+	
+	bufferedReader.close();
+}catch(IOException ioException){
+	ioException.printStackTrace();
+}*/
