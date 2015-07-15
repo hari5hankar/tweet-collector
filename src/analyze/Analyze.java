0bf4922cd806a60a23994a9b055cb9e46a376bce
harishankar-vishwanathan <harishankar.vishwanathan@gmail.com>
2015-07-14 21:04:33 -0400

    analysis for correlation b/w #retweets, #followers, #following

--- src/analyze/Analyze.java
+++ src/analyze/Analyze.java
@@ -3,41 +3,57 @@ package analyze;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
 
 public class Analyze {
 
-	static File rootDirectory = new File("C:\\Users\\Security\\Workspace2\\tweet-collector\\data\\");
+	File rootDirectory;
+	long root;
+	int round;
+
+	static int apiCallCount = 0;
+	static int totalCallCount = 0;
+
 	int totalRetweeters = 0;
 	int totalTweets = 0;
 	int totalMostActiveRetweeters = 0;
 	int totalMostActiveFollowerRetweeters = 0;
 	int totalRetweetedInNextRound = 0;
-
-	public Analyze(int round) {
-
-		goDeeper(rootDirectory, round);
-		System.out.println(this.totalRetweeters);
-		System.out.println(this.totalTweets);
-		System.out.println(this.totalMostActiveRetweeters);
-		System.out.println(this.totalMostActiveFollowerRetweeters);
-		System.out.println(this.totalRetweetedInNextRound);
+	File retweetsFollowersFollowingFile;
+	
+	public Analyze(long root) {
+		this.root = root;
+		this.rootDirectory = new File("data\\" + Long.toString(this.root) + "\\");
+		System.out.println(rootDirectory.getAbsolutePath());
 	}
+
+	public void analyze(int round) {
+		this.round = round;
+		this.retweetsFollowersFollowingFile = new File(
+				"analysis\\" + Integer.toString(round) + "\\" + Long.toString(root) + "_RFF_DONE.csv");
+		goDeeper(rootDirectory, round);
+	}
+
 
 	public void goDeeper(File directory, int level) {
 
-		if (level == 0) {
+		if (level == 1) {
 
-			File allRetweetersFile = new File(directory.getAbsolutePath() + "\\" + directory.getName() + "_NV.csv");
-			totalTweets(allRetweetersFile);
-			totalRetweeters(allRetweetersFile);
-			File mostActiveRetweetersFile = new File(directory.getAbsolutePath() + "\\" + directory.getName() + "_NV_MA5.csv");
-			totalMostActiveRetweeters(mostActiveRetweetersFile);
-			File mostActiveFollowerRetweetersFile = new File(directory.getAbsolutePath() + "\\" + directory.getName() + "_MA5_V.csv");
-			totalMostActiveFollowerRetweeters(mostActiveFollowerRetweetersFile);
-			totalRetweetedInNextRound(directory);
-			
+			retweetsFollowersFollowingDone(directory);
+			/*
+			 * totalTweets(directory); totalRetweeters(directory);
+			 * totalMostActiveRetweeters(directory);
+			 * totalMostActiveFollowerRetweeters(directory);
+			 * totalRetweetedInNextRound(directory);
+			 */
 			return;
 		}
 
@@ -50,10 +66,28 @@ public class Analyze {
 		}
 	}
 
-	public void totalTweets (File file) {
+	private int countLines(File file) {
 		int total = 0;
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			String line;
+			line = br.readLine();
+			while (line != null) {
+				total++;
+				line = br.readLine();
+			}
+			br.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		return total;
+	}
+
+	public int totalTweets(File directory) {
+		File allRetweetersFile = new File(directory.getAbsolutePath() + "\\" + directory.getName() + "_NV.csv");
+		int total = 0;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(allRetweetersFile)));
 			String line;
 			line = br.readLine();
 			while (line != null) {
@@ -66,65 +100,109 @@ public class Analyze {
 			ioe.printStackTrace();
 		}
 		this.totalTweets += total;
-	}
-	
-	public void totalRetweeters(File file) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			String line;
-			line = br.readLine();
-			while (line != null) {
-				this.totalRetweeters++;
-				line = br.readLine();
-			}
-			br.close();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-	
-	public void totalMostActiveRetweeters(File file) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			String line;
-			line = br.readLine();
-			while (line != null) {
-				this.totalMostActiveRetweeters++;
-				line = br.readLine();
-			}
-			br.close();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		
-	}
-	
-	public void totalMostActiveFollowerRetweeters(File file) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			String line;
-			line = br.readLine();
-			while (line != null) {
-				this.totalMostActiveFollowerRetweeters++;
-				line = br.readLine();
-			}
-			br.close();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		
+		return total;
 	}
 
-	public void totalRetweetedInNextRound(File directory) {
-		for(File file : directory.listFiles()) {
+	public int totalRetweeters(File directory) {
+		File allRetweetersFile = new File(directory.getAbsolutePath() + "\\" + directory.getName() + "_NV.csv");
+		int total = countLines(allRetweetersFile);
+		this.totalRetweeters += total;
+		return total;
+	}
+
+	public int totalMostActiveRetweeters(File directory) {
+		File mostActiveRetweetersFile = new File(
+				directory.getAbsolutePath() + "\\" + directory.getName() + "_NV_MA5.csv");
+		int total = countLines(mostActiveRetweetersFile);
+		this.totalMostActiveRetweeters += total;
+		return total;
+
+	}
+
+	public int totalMostActiveFollowerRetweeters(File directory) {
+		File mostActiveFollowerRetweetersFile = new File(
+				directory.getAbsolutePath() + "\\" + directory.getName() + "_MA5_V.csv");
+		int total = countLines(mostActiveFollowerRetweetersFile);
+		this.totalMostActiveFollowerRetweeters += total;
+		return total;
+	}
+
+	public int totalRetweetedInNextRound(File directory) {
+		int total = 0;
+		for (File file : directory.listFiles()) {
 			if (file.isDirectory()) {
-				this.totalRetweetedInNextRound++;
+				total++;
 			}
 		}
+		this.totalRetweetedInNextRound += total;
+		return total;
 	}
+
+	public void retweetsFollowersFollowingDone(File directory) {
+		Twitter twitter = new TwitterFactory(authorization.ConfigBuilder.getConfig()).getInstance();
+
+		try {
+			File mostActiveFollowersFile = new File(
+					directory.getAbsolutePath() + "\\" + directory.getName() + "_MA5_V.csv");
+			System.out.println(mostActiveFollowersFile.getAbsolutePath());
+			
+			PrintWriter pw = new PrintWriter(new FileWriter(this.retweetsFollowersFollowingFile, true));
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(mostActiveFollowersFile)));
+			String line;
+			int followers;
+			int following;
+			int retweets;
+			line = br.readLine();
+			while (line != null) {
+				String[] tokens = line.split(",");
+				line = br.readLine();
+
+				followers = 0;
+				following = 0;
+				retweets = Integer.parseInt(tokens[1]);
+				long id = Long.parseLong(tokens[0]);
+
+				apiCallCount++;
+				totalCallCount++;
+				System.out.println("    count: api:  " + apiCallCount + " total:  " + totalCallCount);
+
+				if (apiCallCount == 179) {
+					try {
+						System.out.println("    sleeping...");
+						Thread.sleep(900000);
+						apiCallCount = 0;
+					} catch (InterruptedException inte) {
+						inte.printStackTrace();
+					}
+				}
+
+				try {
+					User user = twitter.showUser(id);
+					followers = user.getFollowersCount();
+					following = user.getFriendsCount();
+				} catch (TwitterException te) {
+					te.printStackTrace();
+				}
+
+				pw.println(id + "," + retweets + "," + followers + "," + following);
+				System.out.println(id + "," + retweets + "," + followers + "," + following);
+				pw.flush();
+			}
+			br.close();
+			pw.close();		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 
-		new Analyze(3);
-
+		Analyze a1 = new Analyze(428333);
+		a1.analyze(3);
+		a1 = new Analyze(813286); 
+		a1.analyze(3); 
+		a1 = new Analyze(21447363);
+		a1.analyze(3);
 	}
 }
--- src/authorization/ConfigBuilder.java
+++ src/authorization/ConfigBuilder.java
@@ -6,35 +6,35 @@ public class ConfigBuilder {
 
         private static Configuration configuration;
 
-     /*  //Abhishek's keys
+/*     //Abhishek's keys
         public static final String consumerKeyStr = "6xApxD6P3xPGa6fGEqeezvBTh";
         public static final String consumerSecretStr ="sZQtcKOlTl9AtTg6ZvMcmuF31EoVk7d7tNqhU8LlwOIcMjMRyP";
         public static final String accessTokenStr ="2584378076-QaUNEp9CY144WwPJXYejRtdi5mjEQJEGeJyUeZ8";
-        public static final String accessTokenSecretStr ="ZB8AIvzEJZvjkedlJMckwHZXabaD3vvh9PTUDa0uhxylQ";
-*/
+        public static final String accessTokenSecretStr ="ZB8AIvzEJZvjkedlJMckwHZXabaD3vvh9PTUDa0uhxylQ";*/
 
-/*        //Omkar's keys
+
+        //Omkar's keys
         public static final String consumerKeyStr = "6kRVFFfsFLfzPH4IUTlvgulbJ";
         public static final String consumerSecretStr = "VwzVBO5ygloqoyRMl9YWSFpiuV5arjO9HpYr648Ci3WEoGD7jl";
         public static final String accessTokenStr ="134179394-f7YpC2Pxd5QXucqoaWg3UAmH1Mc793bmYLhhlPxD";
         public static final String accessTokenSecretStr ="23jbLXADuXY2TA5TUGQkUayPtEqK3Dr5BelH00SKLG6Si";
 
-*/
-        
+
+/*        
         //Harishankar's keys:
         public static final String consumerKeyStr = "RCVjvrIN1nHF9UuddyWIw0GkO";
         public static final String consumerSecretStr ="0D0BeIdeVv8jp7pPBLrhmnas7dlHCEjxxlFLFinFtZWzkut3PQ";
         public static final String accessTokenStr ="2972666885-riSRlSUrmIOZH10msEa7bxOLj4BJVqKaI3GkUzC";
         public static final String accessTokenSecretStr ="pfHalw9HmwFtoVpongAFKod9C4oCzWCGCjeihbvUfiWXe";
-
-/*     
-        //Harishankar's keys:
+*/
+     
+/*       //Harishankar's keys:
         public static final String consumerKeyStr = "Hh9V6RxrIrkgSI1lcFqwPEPAQ";
         public static final String consumerSecretStr ="3bUdrH8LgZ4I525u9rnf17UneKh8RFyu9nuv44r5LmIBFAD4Fx";
         public static final String accessTokenStr ="2972666885-RUy3lbSMgC6oIv6ZDqaL8k7YkAgYTt7PJYKfRnD";
         public static final String accessTokenSecretStr ="Cv7MDgd7UELtCnTJgsWjuQWELCvu8sNSY7fhy6RYoegIU";
-        
-  */      static {
+   */     
+       static {
         	//TODO ask for config number from stdin
         	
 			ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
